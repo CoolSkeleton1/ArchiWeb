a6906c4834c4d45cc90595d8c90989861adece01
masterpapyrus <thepirateking@inbox.ru>
2024-07-14 19:25:18 +0300

    final edits

--- js/main.js
+++ js/main.js
@@ -7,11 +7,24 @@ searchBtn.onclick = () => {
     searchBtn.classList.add("pressed")
     searchBox.classList.add("active");
     cancelBtn.classList.add("active");
-    };
+};
 
 cancelBtn.onclick = () => {
     searchBtn.classList.remove("pressed");
     searchBox.classList.remove("active");
     cancelBtn.classList.remove("active");
     searchInput.value = "";
-    };
+};
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href');
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    })
+}
